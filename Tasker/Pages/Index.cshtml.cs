c35963959483f97de64b29037ddd2b354399056c
Daniel <KasumiL5x@gmail.com>
2020-07-25 21:26:21 +0100

    Namespace fix.

--- Tasker/Pages/Index.cshtml.cs
+++ Tasker/Pages/Index.cshtml.cs
@@ -5,7 +5,7 @@ using System.Threading.Tasks;
 using Microsoft.AspNetCore.Mvc;
 using Microsoft.AspNetCore.Mvc.RazorPages;
 
-namespace MyApp.Namespace
+namespace Tasker.Pages
 {
     public class IndexModel : PageModel
     {
