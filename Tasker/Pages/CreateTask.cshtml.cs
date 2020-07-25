faf9c2f80719a50f51e90cdf3a96a4a6971a9cbc
Daniel <KasumiL5x@gmail.com>
2020-07-25 22:14:15 +0100

    Next task complete (DB linking).

--- Tasker/Pages/CreateTask.cshtml.cs
+++ Tasker/Pages/CreateTask.cshtml.cs
@@ -6,12 +6,20 @@ using Microsoft.AspNetCore.Mvc;
 using Microsoft.AspNetCore.Mvc.RazorPages;
 
 namespace Tasker.Pages {
-    public class CreateTaskModel : PageModel {
-        [BindProperty]
-        public Task NewTask {get; set;}
+public class CreateTaskModel : PageModel {
+  [BindProperty]
+  public Task NewTask {get; set;}
 
-        public IActionResult OnPost() {
-            return RedirectToPage("Index");
-        }
-    }
+  private ApplicationDbContext _context;
+  public CreateTaskModel(ApplicationDbContext context) {
+    this._context = context;
+  }
+
+  public IActionResult OnPost() {
+    _context.Tasks.Add(NewTask);
+    _context.SaveChanges();
+    
+    return RedirectToPage("Index");
+  }
+}
 }
