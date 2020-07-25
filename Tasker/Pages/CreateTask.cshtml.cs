6cc3c0823877b26c2b1003f894529ceba3849e02
Daniel <KasumiL5x@gmail.com>
2020-07-25 21:35:11 +0100

    Next task done.

--- Tasker/Pages/CreateTask.cshtml.cs
+++ Tasker/Pages/CreateTask.cshtml.cs
@@ -5,12 +5,13 @@ using System.Threading.Tasks;
 using Microsoft.AspNetCore.Mvc;
 using Microsoft.AspNetCore.Mvc.RazorPages;
 
-namespace Tasker.Pages
-{
-    public class CreateTaskModel : PageModel
-    {
-        public void OnGet()
-        {
+namespace Tasker.Pages {
+    public class CreateTaskModel : PageModel {
+        [BindProperty]
+        public Task NewTask {get; set;}
+
+        public IActionResult OnPost() {
+            return RedirectToPage("Index");
         }
     }
 }
